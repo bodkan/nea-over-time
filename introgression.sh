32869f1db2774046c7278b28e185e9becbe58bfe
Martin Petr <git@bodkan.net>
2016-10-05 16:44:21 +0200

    Add script for submitting exome introgression simulations to SGE

---
+++ introgression.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+input_dir=clean_data
+sims_dir=simulations
+traject_dir=${sims_dir}/trajectories
+tmp_dir=tmp
+
+mkdir -p $traject_dir
+
+
+# initial Neanderthal proportions
+init_props="0.01 0.03 0.05 0.07 0.09"
+
+# number of replicates of each model
+num_replicates=10
+
+
+# wrapper for submitting introgression simulations to SGE
+run_introgression() {
+    case $2 in
+	0.5)
+	    mem=7G
+	    ;;
+	0.1)
+	    mem=9G
+	    ;;
+	0.0)
+	    mem=11G
+	    ;;
+	*)
+	    echo "Suspicious dominance coefficient: $2"
+	    exit
+    esac
+
+    run_id=exome_only__h_${2}__init_nea_${5}__rep_${1}
+    
+    qsub -V -cwd -j y -S /bin/bash -l virtual_free=$mem,h_vmem=$mem \
+	 -o ${tmp_dir}/sge__${run_id}.out -N ${run_id} \
+	 run_introgression.py \
+	    --population-file ${sims_dir}/exome_only__h_${2}__seed_*.txt \
+	    --exon-coordinates $3 \
+	    --recomb-map $4 \
+	    --neutral-spacing 10000 \
+	    --admixture-rate $5 \
+	    --dominance-coef $2 \
+	    --output-file ${traject_dir}/${run_id}.txt
+}
+
+
+# 
+# Simulations of Neanderthal ancestry trajectories in the European
+# population using previously simulated populations with accumulated
+# deleterious variants.
+#
+# Exome only, not including sites from the archaic admixture array.
+#
+
+exome_only_exon_coords=${input_dir}/exome_only_exon_coordinates.txt
+exome_only_recomb_map=${input_dir}/exome_only_recombination_map.txt
+
+
+for rep_i in `seq 1 $num_replicates`; do
+    for init_nea in $init_props; do
+	run_introgression $rep_i 0.5 $exome_only_exon_coords $exome_only_recomb_map $init_nea
+	run_introgression $rep_i 0.1 $exome_only_exon_coords $exome_only_recomb_map $init_nea
+	run_introgression $rep_i 0.0 $exome_only_exon_coords $exome_only_recomb_map $init_nea
+    done
+done
+
+
+# 
+# Simulations of Neanderthal ancestry trajectories in the European
+# population using previously simulated populations with accumulated
+# deleterious variants.
+#
+# Exome only, not including sites from the archaic admixture array.
+#
+# Using SLiM recombination map by Harris and Nielsen, 2016.
+#
+
+# harris_recomb_map=${input_dir}/harris_recombination_map.txt
+# harris_exon_coords=${input_dir}/harris_exon_coordinates.txt
+
+
+# for rep_i in `seq 1 $num_replicates`; do
+#     for init_nea in $init_props; do
+# 	run_introgression $rep_i 0.5 $harris_exon_coords $harris_recomb_map $init_nea
+# 	run_introgression $rep_i 0.1 $harris_exon_coords $harris_recomb_map $init_nea
+# 	run_introgression $rep_i 0.0 $harris_exon_coords $harris_recomb_map $init_nea
+#     done
+# done
