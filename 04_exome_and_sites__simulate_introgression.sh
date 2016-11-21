e27d224731aa90134c0c9acf935c4b41826c1967
Martin Petr <git@bodkan.net>
2016-11-21 10:41:08 +0100

    Update the pipeline to simulate all three demographic models

--- 04_exome_and_sites__simulate_introgression.sh
+++ 04_exome_and_sites__simulate_introgression.sh
@@ -13,7 +13,7 @@ mkdir -p $traject_dir
 init_nea=0.1
 
 # number of replicates of each model
-num_replicates=1
+num_replicates=20
 
 # dates of samples
 dates=`tail -n+2 ${input_dir}/nea_ancestry_direct.tsv | cut -f2 | tr '\n' ' '`
@@ -37,9 +37,9 @@ run_introgression() {
 
     run_id=exonic_and_nonexonic__h_${2}__init_nea_${5}__rep_${1}
     
-#    qsub -V -cwd -j y -S /bin/bash -l virtual_free=$mem,h_vmem=$mem \
-#	 -o ${tmp_dir}/sge__${run_id}.out -N ${run_id} \
-python3	 run_introgression.py \
+    qsub -V -cwd -j y -S /bin/bash -l virtual_free=$mem,h_vmem=$mem \
+	 -o ${tmp_dir}/sge__${run_id}.out -N ${run_id} \
+	 run_introgression.py \
 	    --population-file ${sims_dir}/exome_and_sites__h_${2}__seed_*.txt \
 	    --exon-coordinates $3 \
 	    --recomb-map $4 \
@@ -48,8 +48,9 @@ python3	 run_introgression.py \
 	    --sampling-times $dates \
 	    --exonic-sites $6 \
 	    --nonexonic-sites $7 \
-	    --save-mutations \
-	    --output-prefix ${traject_dir}/${run_id}
+	    --output-prefix ${traject_dir}/${run_id} \
+	    --model $8 \
+	    $9
 }
 
 
@@ -67,7 +68,12 @@ exome_only_recomb_map=${clean_dir}/exome_and_sites_recombination_map.txt
 exonic_sites=${clean_dir}/admixture_array_coordinates_exonic.txt
 nonexonic_sites=${clean_dir}/admixture_array_coordinates_nonexonic.txt
 
-for rep_i in `seq 1 $num_replicates`; do
-    run_introgression $rep_i 0.5 $exome_only_exon_coords $exome_only_recomb_map $init_nea $exonic_sites $nonexonic_sites
-exit
+for model in 'constant' 'linear' 'gravel'; do
+    run_introgression 1 0.5 $exome_only_exon_coords $exome_only_recomb_map $init_nea $exonic_sites $nonexonic_sites $model --save-mutations
+done
+
+for rep_i in `seq 2 $num_replicates`; do
+    for model in 'constant' 'linear' 'gravel'; do
+        run_introgression $rep_i 0.5 $exome_only_exon_coords $exome_only_recomb_map $init_nea $exonic_sites $nonexonic_sites $model
+    done
 done
