7f1f7adbb58f6bf4fbb934ac4158d00ec7ae0cd3
Martin Petr <git@bodkan.net>
2017-01-17 14:44:39 +0100

    Add filtering and random calling, fix ref/alt column issues

--- R/process_eigenstrat.R
+++ R/process_eigenstrat.R
@@ -17,7 +17,9 @@
 
 
 library(tidyverse)
+library(magrittr)
 library(stringr)
+library(BSgenome.Hsapiens.UCSC.hg19)
 
 args <- commandArgs(trailingOnly=TRUE)
 if (length(args) != 4) {
@@ -31,24 +33,34 @@ output_file <- "t.tsv" #args[4]
 
 snp <- read_fwf(snp_file, fwf_widths(c(20, 6, 16, 16, 2, 2),
                                      col_names=c("id", "chrom", "gen", "pos",
-                                                 "ref", "alt")))
+                                                 "alt", "ref")))
 ind <- read_table(ind_file, col_names=c("id", "sex", "label"))
 geno <- readLines(geno_file) %>% str_split("", simplify=TRUE)
 
 # replace dashes and dots in sample names with underscores
 colnames(geno) <- ind$id %>% str_replace("-|\\.", "_")
 
-snp %>%
+# perform random calling on het calls - for each het call (1), flip a
+# coin and replace it with 0 or 2
+hets <- geno == 1
+geno <- replace(geno, hets, 2 * rbinom(sum(hets), 1, 0.5))
+
+# merge the geno/snp file data into a single dataframe
+snp_table <-
+    snp %>%
     select(chrom, pos, ref, alt) %>% # select only columns of interest
-    bind_cols(as_tibble(geno)) %>%   # merge with the snp calls
-    write_tsv(output_file)
+    bind_cols(as_tibble(geno))       # merge with the snp calls
 
+# fetch hg19 reference alleles at positions from the array
+hg19_refs <-
+    mutate(snp_table, start=pos, end=start, chrom=paste0("chr", chrom)) %>%
+    makeGRangesFromDataFrame %>% # convert dataframe to GRanges
+    getSeq(Hsapiens, .) %>%      # fetch hg19 alleles at sites in the GRanges object
+    as.vector
 
-x <- read_tsv("../tmp/t.tsv")
-y <- read_tsv("../tmp/i.tsv")
-colnames(y) <- str_replace(colnames(y), "Dolni", "Vestonice")
+# select only those SNPs from the EIGENSTRAT data where the true
+# hg19 allele matches the reference in the data (some 1% of sites
+# seem to have ref/alt columns flipped)
+snp_table %<>% filter(ref == hg19_refs)
 
-ids <- colnames(x)[5:length(colnames(x))]
-
-xi <- sapply(ids, function(i) table(x[i]))
-yi <- sapply(ids, function(i) table(y[i]))
+write_tsv(snp_table, output_file)
