2823266025032afe72153226246ee4c1391535eb
Martin Petr <git@bodkan.net>
2017-08-08 23:20:16 +0200

    Fix syntax error

--- admixtools/admixtools.R
+++ admixtools/admixtools.R
@@ -45,7 +45,7 @@ read_dstats <- function(log_filename) {
         paste(collapse="\n") %>%
         textConnection %>%
         read.table %>%
-        .[c(1:6, ncol(.) - 2, ncol(.) - 1, ncol(.)] %>%
+        .[c(1:6, ncol(.) - 2, ncol(.) - 1, ncol(.))] %>%
         setNames(c("W", "X", "Y", "Z", "Dstat", "Zscore", "BABA", "ABBA", "n_snps"))
 
     res
