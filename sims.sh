cb45abfbbae535d1974ec851632b4a3dad65e202
Martin Petr <git@bodkan.net>
2017-11-16 01:04:40 +0100

    Update the minipipeline

--- sims.sh
+++ sims.sh
@@ -4,12 +4,12 @@ mkdir data/burnins
 
 for region in protein_coding promoter utr3 tf_binding_site; do
 for h in 0.0 0.5 1.0; do
-	echo python3 code/run_mutation_accumulation.py \
+	python3 code/run_mutation_accumulation.py \
 		--regions data/slim_coords/${region}_regions.bed \
 		--sites data/slim_coords/${region}_sites.bed \
 		--recomb-map data/slim_coords/${region}_recomb_map.bed \
 		--mut-rate 1e-8 \
 		--dominance-coef $h \
-		--output data/burnins/${region}_${h}.out
+		--output data/burnins/${region}_${h}.out &
 done
 done
