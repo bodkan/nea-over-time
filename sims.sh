8c7160e9b67b585fc4e8e1d79171689a153ee49b
Martin Petr <git@bodkan.net>
2017-11-16 01:30:01 +0100

    Update sim script

--- sims.sh
+++ sims.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir data/burnins
+mkdir -p data/burnins
 
 for region in protein_coding promoter utr3 tf_binding_site; do
 for h in 0.0 0.5 1.0; do
-	python3 code/run_mutation_accumulation.py \
+	echo python3 code/run_mutation_accumulation.py \
 		--regions data/slim_coords/${region}_regions.bed \
 		--sites data/slim_coords/${region}_sites.bed \
 		--recomb-map data/slim_coords/${region}_recomb_map.bed \
