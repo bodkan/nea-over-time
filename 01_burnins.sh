af787e888cd1e3de32f026fb30c15865735d9d9a
Martin Petr <git@bodkan.net>
2017-11-18 22:47:36 +0100

    Revert the location of burnin files

--- 01_burnins.sh
+++ 01_burnins.sh
@@ -2,7 +2,7 @@
 
 # ----------------------------------------------------------------------
 
-mkdir data/simulations/burnins
+mkdir data/burnins
 
 for region in protein_coding promoter utr3 tf_binding_site; do
 for h in 0.0 0.5 1.0; do
@@ -12,14 +12,14 @@ for h in 0.0 0.5 1.0; do
         --recomb-map data/slim_coords/${region}_recomb_map.bed \
         --mut-rate 1e-8 \
         --dominance-coef $h \
-        --output data/simulations/burnins/${region}_h_${h}.txt &
+        --output data/burnins/${region}_h_${h}.txt &
 done
 done
 
 
 # ----------------------------------------------------------------------
 
-mkdir data/simulations/burnins/toy_genome
+mkdir data/burnins/toy_genome
 
 for h in `seq 0.0 0.1 1.0`; do
     python3 code/run_mutation_accumulation.py \
@@ -28,7 +28,7 @@ for h in `seq 0.0 0.1 1.0`; do
         --recomb-map data/slim_coords/toy_genome/independent_haps_recomb_map.bed \
         --mut-rate 1e-8 \
         --dominance-coef $h \
-        --output data/simulations/burnins/toy_genome/independent_haps_h_${h}.txt &
+        --output data/burnins/toy_genome/independent_haps_h_${h}.txt &
 done
 python3 code/run_mutation_accumulation.py \
     --regions data/slim_coords/toy_genome/independent_haps_regions.bed \
@@ -36,7 +36,7 @@ python3 code/run_mutation_accumulation.py \
     --recomb-map data/slim_coords/toy_genome/independent_haps_recomb_map.bed \
     --mut-rate 0 \
     --dominance-coef 0.0 \
-    --output data/simulations/burnins/toy_genome/independent_haps_neutral.txt &
+    --output data/burnins/toy_genome/independent_haps_neutral.txt &
 
 
 # ----------------------------------------------------------------------
@@ -48,7 +48,7 @@ for h in `seq 0.0 0.1 1.0`; do
         --recomb-map data/slim_coords/toy_genome/linked_haps_recomb_map.bed \
         --mut-rate 1e-8 \
         --dominance-coef $h \
-        --output data/simulations/burnins/toy_genome/linked_haps_h_${h}.txt &
+        --output data/burnins/toy_genome/linked_haps_h_${h}.txt &
 done
 python3 code/run_mutation_accumulation.py \
     --regions data/slim_coords/toy_genome/linked_haps_regions.bed \
@@ -56,4 +56,4 @@ python3 code/run_mutation_accumulation.py \
     --recomb-map data/slim_coords/toy_genome/linked_haps_recomb_map.bed \
     --mut-rate 0 \
     --dominance-coef 0.0 \
-    --output data/simulations/burnins/toy_genome/linked_haps_neutral.txt &
+    --output data/burnins/toy_genome/linked_haps_neutral.txt &
