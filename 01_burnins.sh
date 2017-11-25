6ee1f072e27caf3c6fef6fcf826d87aae6f9e4cb
Martin Petr <git@bodkan.net>
2017-11-25 23:52:59 +0100

    Add burnin with unif recomb rate

--- 01_burnins.sh
+++ 01_burnins.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
-# ----------------------------------------------------------------------
-
 mkdir data/burnins
+
+# all chromosomes ------------------------------------------------------------
 
 for region in protein_coding promoter utr3 tf_binding_site; do
 for h in 0.0 0.5 1.0; do
@@ -16,11 +14,11 @@ for h in 0.0 0.5 1.0; do
 done
 done
 
-# ----------------------------------------------------------------------
+# chromosomes 1-5 only -------------------------------------------------------
 
 mkdir data/slim_coords/subset
 
-# generate chr1-5 subsets of the coordinate files 
+# generate chr1-5 subsets of the coordinate files - used to get faster burnins
 for f in data/slim_coords/*.bed; do
     head -n 1 $f > data/slim_coords/subset/`basename ${f}`
     for chr in `seq 1 5`; do
@@ -37,6 +35,20 @@ for h in `seq 0.0 0.1 1.0`; do
         --mut-rate 1e-8 \
         --dominance-coef $h \
         --output data/burnins/subset_${region}_h_${h}.txt &
+done
+done
+
+# uniform recombination rate - to test if the slower recombination rate really
+# leads to a more steep decline - as observed in the old simulations
+for region in protein_coding promoter utr3 tf_binding_site; do
+for h in 0.5; do
+    python3 code/run_mutation_accumulation.py \
+        --regions data/slim_coords/subset/${region}_regions.bed \
+        --sites data/slim_coords/subset/${region}_sites.bed \
+        --recomb-map data/slim_coords/subset/uniform_${region}_recomb_map.bed \
+        --mut-rate 1e-8 \
+        --dominance-coef $h \
+        --output data/burnins/uniform_subset_${region}_h_${h}.txt &
 done
 done
 
