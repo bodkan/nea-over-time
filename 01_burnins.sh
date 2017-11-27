8f75458fb094e7554b646e7570a2e4d86172ac29
Martin Petr <git@bodkan.net>
2017-11-27 18:08:10 +0100

    Add uniform whole genome burnins

--- 01_burnins.sh
+++ 01_burnins.sh
@@ -16,7 +16,7 @@ for region in protein_coding promoter utr3 tf_binding_site; do
 for h in `seq 0.0 0.1 1.0`; do
     python3 code/run_mutation_accumulation.py \
         --regions data/slim_coords/subset/${region}_regions.bed \
-        --sites data/slim_coords/subset/${region}_sites.bed \
+        --sites data/slim_coords/subset/${region}_all_sites.bed \
         --recomb-map data/slim_coords/subset/${region}_recomb_map.bed \
         --mut-rate 1e-8 \
         --dominance-coef $h \
@@ -30,7 +30,7 @@ for region in protein_coding promoter utr3 tf_binding_site; do
 for h in 0.5; do
     python3 code/run_mutation_accumulation.py \
         --regions data/slim_coords/subset/${region}_regions.bed \
-        --sites data/slim_coords/subset/${region}_sites.bed \
+        --sites data/slim_coords/subset/${region}_all_sites.bed \
         --recomb-map data/slim_coords/subset/uniform_${region}_recomb_map.bed \
         --mut-rate 1e-8 \
         --dominance-coef $h \
@@ -40,25 +40,28 @@ done
 
 
 
-
-
-
-
-
-
-
-
-
 # all chromosomes ------------------------------------------------------------
 
 for region in protein_coding promoter utr3 tf_binding_site; do
 for h in 0.0 0.5 1.0; do
     python3 code/run_mutation_accumulation.py \
         --regions data/slim_coords/${region}_regions.bed \
-        --sites data/slim_coords/${region}_sites.bed \
+        --sites data/slim_coords/${region}_all_sites.bed \
         --recomb-map data/slim_coords/${region}_recomb_map.bed \
         --mut-rate 1e-8 \
         --dominance-coef $h \
         --output data/burnins/${region}_h_${h}.txt &
+done
+done
+
+for region in protein_coding promoter utr3 tf_binding_site; do
+for h in 0.5; do
+    python3 code/run_mutation_accumulation.py \
+        --regions data/slim_coords/${region}_regions.bed \
+        --sites data/slim_coords/${region}_all_sites.bed \
+        --recomb-map data/slim_coords/uniform_${region}_recomb_map.bed \
+        --mut-rate 1e-8 \
+        --dominance-coef $h \
+        --output data/burnins/uniform_${region}_h_${h}.txt &
 done
 done
