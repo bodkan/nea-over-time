75e6f945bc508f44b05a3347a6500c040d3c0144
Martin Petr <git@bodkan.net>
2017-12-21 16:13:11 +0100

    Update the processing pipeline with new Altai/Vindija/Mez1/Denisovan

--- 00_data_processing.sh
+++ 00_data_processing.sh
@@ -7,22 +7,31 @@ mkdir -p $eigenstrat_dir
 cd $eigenstrat_dir
 
 # download Fu et al. data
-mkdir Fu
-cd Fu
+mkdir iceage
+cd iceage
 wget https://reich.hms.harvard.edu/sites/reich.hms.harvard.edu/files/inline-files/FuQ.zip
 unzip FuQ.zip
 cd ../../../
 
 # extract the coordinates of archaic admixture array sites (for later)
 mkdir -p data/bed
-awk -v OFS="\t" '{print $2, $4 - 1, $4}' ${eigenstrat_dir}/Fu/archaic.snp > data/bed/admixture_array.bed
+awk -v OFS="\t" '{print $2, $4 - 1, $4}' ${eigenstrat_dir}/iceage/archaic.snp > data/bed/admixture_array.bed
 
 gt_dir=data/genotypes
 mkdir -p $gt_dir
 
 # process Fu et al. data into a simple 0/1/2 table
-Rscript code/process_eigenstrat.R ${eigenstrat_dir}/Fu/archaic.geno ${eigenstrat_dir}/Fu/archaic.snp ${eigenstrat_dir}/Fu/archaic.ind ${gt_dir}/ice_age.tsv
+Rscript code/process_eigenstrat.R ${eigenstrat_dir}/iceage/archaic.geno ${eigenstrat_dir}/iceage/archaic.snp ${eigenstrat_dir}/iceage/archaic.ind ${gt_dir}/ice_age.tsv
 chmod -w ${gt_dir}/ice_age.tsv
+
+
+
+
+
+
+
+########
+# fix this to use the newest merge of archaics & SGDP data from Steffi
 
 # get the list of SGDP (C-team)
 sgdp_samples=`bcftools query -l /mnt/sequencedb/SGDP_May2016/combined_vcf/c_team_chr1.vcf.gz | grep "^S_" | tr '\n' ' '`
@@ -47,10 +56,14 @@ chmod -w ${gt_dir}/archaics.tsv
 
 
 
+
+
+
+
 # ---------------------------------------------------------------------- 
 #  get the coordinates of the Big Yoruba array sites
 cp /r1/people/public/AncientDNA/probe_designs/AA77-81_bigYoruba/big_yoruba_and_altai_filtN_printed_probes_with_annotation.txt data/
-awk -v OFS="\t" '{print $1, $2 - 1, $2}' data/big_yoruba_and_altai_filtN_printed_probes_with_annotation.txt > data/bed/big_yoruba_array.bed
+awk -v OFS="\t" '{print $1, $2 - 1, $2}' data/big_yoruba_and_altai_filtN_printed_probes_with_annotation.txt > data/bed/bigyri_array.bed
 
 
 
@@ -58,123 +71,69 @@ awk -v OFS="\t" '{print $1, $2 - 1, $2}' data/big_yoruba_and_altai_filtN_printed
 # generate updated EIGENSTRAT 2.2 M sites data that include the new
 # processing of Altai, Vindija and Denisova (genotyped using snpAD)
 
+mkdir data/eigenstrat/bigyri_ho
+cd data/eigenstrat/bigyri_ho
+
 # make a copy of Qiaomei's combined Eigenstrat dataset
-mkdir data/eigenstrat/big_yoruba_ho; cd data/eigenstrat/big_yoruba_ho;
 cp /mnt/454/Carbon_beast_QM/TY/snp/UPA_all.{snp,ind,geno} .
 
 # make a directory for different coordinate files
-less UPA_all.snp | awk -v OFS="\t" '{print $2, $4-1, $4}' > ../coordinates/2.2M.bed
+less UPA_all.snp | awk -v OFS="\t" '{print $2, $4-1, $4}' > ../../bed/2.2M.bed
 
-# ---------------------------------------------------------------------- 
-# generate the new high coverage Vindija "geno" and "snp" files
-
-seq 1 22 | xargs -P 22 -I {} bash -c "bcftools view -R 2.2M.pos -M 2 /mnt/454/Vindija/high_cov/genotypes/Vindija33.19/chr{}_mq25_mapab100.vcf.gz | bcftools query -f '%CHROM\t%POS\t[%GT]\n' | sed 's/0\/0/2/g; s/0\/1/1/g; s/1\/1/0/g' > chr{}.tmp"
-cat chr{1..22}.tmp > vindija.tmp
+seq 1 22 | xargs -P 22 -I {} bash -c "bcftools view -a /mnt/scratch/steffi/D/Vcfs/mergedArchModernApes/merged_high_apes_low_sgdp1_chr{}.vcf.gz -R ../../bed/2.2M.bed -s AltaiNeandertal,Vindija33.19,Mezmais1Deam,Denisova | bcftools query -f '%CHROM\t%POS[\t%GT]\n' | sed 's/\.\/\./9/g; s/0\/0/0/g; s/0\/1/1/g; s/1\/1/2/g' | grep -v '/' > chr{}.tmp"
+cat chr{1..22}.tmp > all_chr.tmp
 rm chr*.tmp
 
+R --no-save < <(echo '
 library(tidyverse)
-all <- read_table2("UPA_all.snp", col_names=c("id", "chrom", "gen", "pos", "alt", "ref"))
-vindija <- read_tsv("vindija.tmp", col_names=c("chrom", "pos", "geno"))
-merged <- left_join(all, vindija)
-merged$geno[is.na(merged$geno)] <- 9
-write_tsv(select(merged, -geno), "vindija.snp", col_names=FALSE)
-write_tsv(select(merged, geno), "vindija.geno", col_names=FALSE)
+library(admixr)
+all <- read_snp("UPA_all.snp")
+archaics <- read_tsv("asd.tmp", col_names=c("chrom", "pos", "Altai", "Vindija", "Mez1", "Denisovan"), col_types="ciiiii")
+joined <- left_join(all, archaics)
+joined$Altai[is.na(joined$Altai)] <- 9
+joined$Vindija[is.na(joined$Vindija)] <- 9
+joined$Mez1[is.na(joined$Mez1)] <- 9
+joined$Denisovan[is.na(joined$Denisovan)] <- 9
 
-
-# create 'ind' EIGENSTRAT file
-echo "new_Vindija F new_Vindija" > vindija.ind
+write_geno("archaics.geno", select(joined, Altai:Denisovan))
+write_snp("archaics.snp", select(joined, -(Altai:Denisovan)))
+write_ind("archaics.ind", tibble(name=c("new_Altai", "new_Vindija", "new_Mez1", "new_Densiovan"), sex=rep("F", 4), pop=c("new_Altai", "new_Vindija", "new_Mez1", "new_Denisovan")))
+')
 
 # generate a mergit parameter file
 echo "outputformat: EIGENSTRAT
 geno1: UPA_all.geno
 snp1: UPA_all.snp
 ind1: UPA_all.ind
-geno2: vindija.geno
-snp2: vindija.snp
-ind2: vindija.ind
-genooutfilename: UPA_vi.geno
-snpoutfilename: UPA_vi.snp
-indoutfilename: UPA_vi.ind" > mergeit_vi.par
-
-mergeit -p mergeit_vi.par
-
-
-# ---------------------------------------------------------------------- 
-# generate the new high coverage Altai "geno" and "snp" files
-seq 1 22 | xargs -P 22 -I {} bash -c "bcftools view -R 2.2M.pos -M 2 /mnt/454/Vindija/high_cov/genotypes/Altai/chr{}_mq25_mapab100.vcf.gz | bcftools query -f '%CHROM\t%POS\t[%GT]\n' | sed 's/0\/0/2/g; s/0\/1/1/g; s/1\/1/0/g' > chr{}.tmp"
-cat chr{1..22}.tmp > altai.tmp
-rm chr*.tmp
-
-library(tidyverse)
-all <- read_table2("UPA_all.snp", col_names=c("id", "chrom", "gen", "pos", "alt", "ref"))
-altai <- read_tsv("altai.tmp", col_names=c("chrom", "pos", "geno"))
-merged <- left_join(all, altai)
-merged$geno[is.na(merged$geno)] <- 9
-write_tsv(select(merged, -geno), "altai.snp", col_names=FALSE)
-write_tsv(select(merged, geno), "altai.geno", col_names=FALSE)
-
-
-# create 'ind' EIGENSTRAT file
-echo "new_Altai F new_Altai" > altai.ind
-
-# generate a mergit parameter file
-echo "outputformat: EIGENSTRAT
-geno1: UPA_merged.geno
-snp1: UPA_merged.snp
-ind1: UPA_merged.ind
-geno2: altai.geno
-snp2: altai.snp
-ind2: altai.ind
-genooutfilename: UPA_vialtai.geno
-snpoutfilename: UPA_vialtai.snp
-indoutfilename: UPA_vialtai.ind" > mergeit_vialtai.par
-
-mergeit -p mergeit_vialtai.par
-
-
-
-# ---------------------------------------------------------------------- 
-# generate the new high coverage Denisova "geno" and "snp" files
-seq 1 22 | xargs -P 22 -I {} bash -c "bcftools view -R 2.2M.pos -M 2 /mnt/454/Vindija/high_cov/genotypes/Denisova/chr{}_mq25_mapab100.vcf.gz | bcftools query -f '%CHROM\t%POS\t[%GT]\n' | sed 's/0\/0/2/g; s/0\/1/1/g; s/1\/1/0/g' > chr{}.tmp"
-cat chr{1..22}.tmp > denisova.tmp
-rm chr*.tmp
-
-library(tidyverse)
-all <- read_table2("UPA_all.snp", col_names=c("id", "chrom", "gen", "pos", "alt", "ref"))
-denisova <- read_tsv("denisova.tmp", col_names=c("chrom", "pos", "geno"))
-merged <- left_join(all, denisova)
-merged$geno[is.na(merged$geno)] <- 9
-write_tsv(select(merged, -geno), "denisova.snp", col_names=FALSE)
-write_tsv(select(merged, geno), "denisova.geno", col_names=FALSE)
-
-
-# create 'ind' EIGENSTRAT file
-echo "new_Denisova F new_Denisova" > denisova.ind
-
-# generate a mergit parameter file
-echo "outputformat: EIGENSTRAT
-geno1: UPA_merged_all.geno
-snp1: UPA_merged_all.snp
-ind1: UPA_merged_all.ind
-geno2: denisova.geno
-snp2: denisova.snp
-ind2: denisova.ind
+geno2: archaics.geno
+snp2: archaics.snp
+ind2: archaics.ind
 genooutfilename: UPA_merged.geno
 snpoutfilename: UPA_merged.snp
-indoutfilename: UPA_merged.ind" > mergeit_merged.par
+indoutfilename: UPA_merged.ind" > mergeit_archaics.par
 
-mergeit -p mergeit_merged.par
-
-
+mergeit -p mergeit_archaics.par
 
 
-# # get the merged VCFs of SGDP + archaics
-# mkdir raw_data/merged_vcfs
+R --no-save < <(echo '
+library(tidyverse)
+library(admixr)
 
-# for f in /mnt/scratch/steffi/D/Vcfs/mergedArchaics_SGDP0/*chr{1..22}.vcf.gz; do zcat $f | ./filter_vcf_with_bed.py <(awk -vOFS="\t" '{print $2, $4-1, $4}' raw_data/eigenstrat_all/UPA_merged_all.snp); done | awk '($5 != "." && $5 != "-" && length($5) == 1) { print $0 }' | bgzip > raw_data/merged_vcfs/mergedArchaics_qual0.vcf.gz
+all_data <- read_eigenstrat("UPA_all")
+archaics <- read_tsv("archaics.tmp", col_names=c("chrom", "pos", "Altai", "Vindija", "Mez1", "Denisovan"), col_types="ciiiii")
 
-# for f in /mnt/scratch/steffi/D/Vcfs/mergedArchaics/*chr{1..22}.vcf.gz; do zcat $f | ./filter_vcf_with_bed.py <(awk -vOFS="\t" '{print $2, $4-1, $4}' raw_data/eigenstrat_all/UPA_merged_all.snp); done | awk '($5 != "." && $5 != "-" && length($5) == 1) { print $0 }' | bgzip > raw_data/merged_vcfs/mergedArchaics_qual1.vcf.gz
+joined <- left_join(all_data$snp, archaics)
+joined$Altai[is.na(joined$Altai)] <- 9
+joined$Vindija[is.na(joined$Vindija)] <- 9
+joined$Mez1[is.na(joined$Mez1)] <- 9
+joined$Denisovan[is.na(joined$Denisovan)] <- 9
 
+write_geno("archaics.geno", select(joined, Altai:Denisovan))
+write_snp("all.snp", all_data$snp)
+write_ind("all.ind", bind_rows(all_data$ind, tibble(id=c("new_Altai", "new_Vindija", "new_Mez1", "new_Densiovan"), sex=rep("F", 4), label=c("new_Altai", "new_Vindija", "new_Mez1", "new_Denisovan"))))
+')
+
+paste -d '' UPA_all.geno archaics.geno > all.geno
 
 
 
@@ -192,8 +151,6 @@ rm bkgd.tar.gz
 # download the hg18-to-hg19 liftover chain
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg18/liftOver/hg18ToHg19.over.chain.gz
 gunzip hg18ToHg19.over.chain.gz
-
-
 
 
 
