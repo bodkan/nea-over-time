1a487f20cccfdb516c8bc34a31c3e7cde0737b5e
Martin Petr <git@bodkan.net>
2018-01-19 19:50:29 +0100

    Run burnins of different Neanderthal Ne

--- 01_burnins.sh
+++ 01_burnins.sh
@@ -2,6 +2,7 @@
 
 mkdir data/burnins
 
+# normal burnins for different regions
 for region in exon protein_coding promoter utr3 tf_binding_site; do
 for h in 0.0 0.5 1.0; do
     python3 code/run_mutation_accumulation.py \
@@ -14,6 +15,7 @@ for h in 0.0 0.5 1.0; do
 done
 done
 
+# burnin of a merge of regions showing functional significance
 region="merged"; h="0.5"
 python3 code/run_mutation_accumulation.py \
     --regions data/slim_coords/${region}_regions.bed \
@@ -22,6 +24,20 @@ python3 code/run_mutation_accumulation.py \
     --mut-rate 1e-8 \
     --dominance-coef $h \
     --output data/burnins/${region}_h_${h}.txt &
+
+# testing the effect of different Neanderthal population sizes
+region="exon"; h=0.5
+for Ne in 100 500 10000; do
+    python3 code/run_mutation_accumulation.py \
+        --regions data/slim_coords/${region}_regions.bed \
+        --sites data/slim_coords/${region}_all_sites.bed \
+        --recomb-map data/slim_coords/${region}_recomb_map.bed \
+        --nea-size $Ne \
+        --mut-rate 1e-8 \
+        --dominance-coef $h \
+        --output data/burnins/nea_Ne_${Ne}_${region}_h_${h}.txt &
+done
+cp data/burnins/exon_h_0.5.txt data/burnins/nea_Ne_1000_exon_h_0.5.txt
 
 # uniform recombination rate test
 for region in exon protein_coding promoter utr3 tf_binding_site; do
