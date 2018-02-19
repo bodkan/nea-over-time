01cd1e23a3fb0425239f7c4424c3f4b55ada5b76
Martin Petr <git@bodkan.net>
2018-02-19 20:37:02 +0100

    Remove whitespace (because OCD)

--- code/genetic_loads.R
+++ code/genetic_loads.R
@@ -2,14 +2,14 @@
 #' neutral Neanderthal markers.
 find_nea_haps <- function(chrom_id, markers, nea_muts) {
   nea_runs <- rle(as.integer(mcols(markers)[[chrom_id]] == 1))
-  
+
   # if there are no N markers, then this haplotype is 100% human
   if (!any(nea_runs$values)) return(NULL)
-  
+
   block_idx <- c(0, nea_runs$lengths %>% cumsum)
   block_start <- block_idx[-length(block_idx)]
   block_end <- block_idx[2:length(block_idx)]
-  
+
   # if there is only one run of N markers, this haplotype is 100% Neanderthal
   nea_pos <- if (length(nea_runs$values) > 1) nea_runs$values == 1 else 1
   hap_start <- block_start[nea_pos]
@@ -18,7 +18,7 @@ find_nea_haps <- function(chrom_id, markers, nea_muts) {
   haps <- GRanges(unique(seqnames(markers)),
                   IRanges(start=start(markers[hap_start + 1, ]),
                           end=start(markers[hap_end, ])))
-  
+
   hits <- findOverlaps(nea_muts[mcols(nea_muts)[[chrom_id]] == 1], haps)
   func_haps <- haps[unique(subjectHits(hits))]
   func_haps$nea_fit <- sapply(split(nea_muts[mcols(nea_muts)[[chrom_id]] == 1][queryHits(hits)]$S, subjectHits(hits)),
@@ -50,7 +50,7 @@ matching_mh_hap <- function(nea_hap, markers, mh_muts) {
 nea_mh_fitness <- function(vcf_path) {
   # vcf_path <- "data/simulations/exon_h_0.5_rep_1_gen_1.vcf.gz"
   vcf <- readVcf(vcf_path)
-  
+
   # load the GTs of neutral markers on each simulated chromosome
   markers <- mut_genotypes(vcf=vcf, mut_type=1)
   # load the GTs of new non-African mutations
@@ -59,13 +59,13 @@ nea_mh_fitness <- function(vcf_path) {
   # the new non-African mutations
   mh_muts <- c(mut_genotypes(vcf=vcf, mut_type=0, pop_origin=1, t_min=70000), new_muts) %>% sort
   nea_muts <- c(mut_genotypes(vcf=vcf, mut_type=0, pop_origin=2), new_muts) %>% sort
-  
+
   # find N haplotypes on each simulated chromosome and calculate their fitness
   nea_haps <- chrom_ids(markers) %>%
     lapply(find_nea_haps, markers, nea_muts) %>%
     compact %>%
     Reduce(c, .)
-  
+
   # find "competing" MH haplotypes for each identified N haplotype
   nea_vs_mh_hap <-
       lapply(nea_haps, matching_mh_hap, markers, mh_muts) %>%
@@ -73,11 +73,11 @@ nea_mh_fitness <- function(vcf_path) {
       as.data.frame %>%
       select(nea_fit, mh_fit) %>%
       as_tibble
-  
+
   nea_vs_mh_hap
 }
 
-# 
+#
 # ggplot(fits, aes(nea_fit, mh_mean)) +
 #     geom_point() +
 #     geom_errorbar(aes(ymin=lower_ci, ymax=upper_ci)) +
