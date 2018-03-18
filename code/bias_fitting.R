dfc238b8dfe285d32e8a7681012fa23195a7afc8
Martin Petr <git@bodkan.net>
2018-03-18 16:47:00 +0100

    Add code for ascertainment bias analysis

--- code/bias_fitting.R
+++ code/bias_fitting.R
@@ -1,15 +1,17 @@
 library(tidyverse)
-library(parallel)
 library(admixstats)
 
 # Plot Neanderthal ancestry over time.
-plot_nea <- function(df, real_lm=NULL, sim_lm=NULL) {
-  p <- ggplot(df, aes(t_admix, nea)) + geom_point(size=5) + xlim(0, 55000) + ylim(0, 0.05)
+plot_nea <- function(df, est_lm = FALSE, real_lm = NULL, sim_lm = NULL) {
+  p <- ggplot(df, aes(t_admix, nea)) + geom_point(alpha = 1/2, size = 5) + xlim(0, 55000) + ylim(0, 0.08)
   if (!is.null(real_lm)) {
     p <- p + geom_abline(intercept=coef(real_lm)[1], slope=coef(real_lm)[2], linetype=2, color="blue")
   }
   if (!is.null(sim_lm)) {
     p <- p + geom_abline(intercept=coef(sim_lm)[1], slope=coef(sim_lm)[2], linetype=2, color="red")
+  }
+  if (est_lm) {
+    p <- p + geom_smooth(method = "lm", linetype = 2, color = "red")
   }
   p
 }
@@ -28,15 +30,96 @@ nea_lm <- function(df) {
   lm(nea ~ t_admix, data=df)
 }
 
-# Calculate the ratio between magnitudes of slopes in real vs simulated data.
-slope_ratio <- function(real_lm, sim_lm) {
-  as.vector(coef(real_lm)[2] / coef(sim_lm)[2])
+direct_props <- function(sites, sample_names) {
+  direct_prop(sites, sample_names) %>% inner_join(select(real_est, -nea), by = "name")
 }
 
-load_abc <- function(path, prefix) {
-  list.files(path, paste0(prefix, ".*"), full.names=TRUE) %>%
-    map_df(readRDS)
+f4_ratios <- function(sites, sample_names) {
+  f4_ratio(sites,
+           x = sample_names,
+           a = c("nea_1", "nea_2"), b = c("nea_3", "nea_4"), c = paste0("afr_", 1:10),
+           o = "chimp_1") %>% inner_join(select(real_est, -nea), by = "name")
 }
+
+real_est <- load_estimates("~/projects/nea-over-time/data/admixture_array_nea.tsv")
+
+library(parallel)
+sims_df <- data.frame()
+for (Ne in c(10000, 20000, 30000, 40000, 50000)) {
+  for (m in c(0, 0.0001, 0.001)) {
+    for (t in c(20000, 15000, 10000, 5000)) {
+
+reps <- mclapply(1:20, mc.cores = 20, function(rep_i) {
+
+sites <- nea_admix_scrm(0.03, 0, m, t, t, Ne_eur = Ne,
+                        n_afr = 200,
+                        n_eur = nrow(filter(real_est, pop != "EMH")),
+                        n_asn = 2, n_nea = 4, n_chimp = 1,
+                        n_haps = 100000, hap_length = 5001,
+                        emh_ages = filter(real_est, pop == "EMH")$age)
+ 
+admix_array <- archaic_admixture_array(sites)
+bigyri_array <- big_yoruba_array(sites)
+ho_array <- human_origins_array(sites)
+
+sample_names <- filter(real_est, !name %in% c("eur_1", "eur_2", "asn_1", "asn_2"))$name
+
+admix <- direct_props(admix_array, sample_names) %>% mutate(sites = "admix")
+bigyri <- f4_ratios(bigyri_array, sample_names) %>% mutate(sites = "bigyri")
+ho <- f4_ratios(ho_array, sample_names) %>% mutate(sites = "ho")
+all <- f4_ratios(sites, sample_names) %>% mutate(sites = "all")
+
+bind_rows(admix, bigyri, ho, all) %>% mutate(Ne = Ne, m = m, t = t, rep = rep_i)
+
+}) %>% bind_rows
+
+sims_df <- bind_rows(sims_df, reps)
+
+    }
+  }
+}
+
+
+
+
+
+
+
+
+# plot_nea(admix, est_lm = T)
+# plot_nea(bigyri, est_lm = T)
+# plot_nea(ho, est_lm = T)
+# plot_nea(all, est_lm = T)
+
+
+
+# nea_admix_scrm(0.03, 1e-4, 1e-4, 15000, 15000, Ne_eur = 20000,
+# n_afr = 10,
+# n_eur = nrow(filter(real_est, pop != "EMH")),
+# n_asn = 6, n_nea = 4, n_chimp = 1,
+# n_haps = 100000, hap_length = 5001,
+# emh_ages = filter(real_est, pop == "EMH")$age, dump_cmd = TRUE) %>% PlotMS(type = "scrm", time.scale = "log10year")
+
+
+
+
+
+
+
+
+
+
+
+# # Calculate the ratio between magnitudes of slopes in real vs simulated data.
+# slope_ratio <- function(real_lm, sim_lm) {
+#   as.vector(coef(real_lm)[2] / coef(sim_lm)[2])
+# }
+# 
+# load_abc <- function(path, prefix) {
+#   list.files(path, paste0(prefix, ".*"), full.names=TRUE) %>%
+#     map_df(readRDS)
+# }
+
 
 
 # real_eur <- load_estimates("data/admixture_array_nea.tsv")
