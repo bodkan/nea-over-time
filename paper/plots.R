c90c64a9f57223a4a4de7e072565fcd56f04f3b0
Martin Petr <git@bodkan.net>
2018-04-10 23:01:53 +0200

    Update plots

--- paper/plots.R
+++ paper/plots.R
@@ -10,17 +10,34 @@ source("paper_theme.R")
 
 
 
+geneflow <- bind_rows(
+  readRDS("rds/grid_scrm_afr-eur.rds") %>% mutate(geneflow = "AFR-EUR"),
+  readRDS("rds/grid_scrm_eur-afr.rds") %>% mutate(geneflow = "EUR-AFR"),
+  readRDS("rds/grid_scrm_bidirect.rds") %>% mutate(geneflow = "both")
+)
 
+filter(geneflow, Ne == 10000, m == 0.0001, t == 15000, sites != "ho") %>%
+  mutate(sites = case_when(sites == "admix" ~ "admixture array",
+                           sites == "all" ~ "all SNPs",
+                           sites == "bigyri" ~ "Big Yoruba")) %>% 
+  ggplot(aes(age, nea)) +
+  geom_point(alpha = 1/2) +
+  geom_smooth(method = "lm", linetype = 2, color = "red") +
+  xlim(40000, 0) + ylim(0, 0.05) +
+  xlab("time before present [years]") + ylab("Neanderthal proportion") +
+  facet_grid(geneflow ~ sites) +
+  paper_theme(axis.text.x = element_text(hjust = 1, angle = 30))
 
 
 
 
 # Neandertal ancestry over time -------------------------------------------
 
-plot_nea_time <- function(df, snp_cutoff = 0, ymax = 0.06, title="") {
+plot_nea_time <- function(df, statistic, array, snp_cutoff = 0, ymax = 0.06, title="") {
   filter(df,
+         stat == statistic,
+         sites == array,
          pop %in% c("EMH", "WestEurasia"),
-         !X %in% near_east,
          snp_count > snp_cutoff) %>% 
     ggplot(aes(age, alpha)) +
     geom_point(aes(size = snp_count), alpha = 1/2) +
@@ -28,56 +45,34 @@ plot_nea_time <- function(df, snp_cutoff = 0, ymax = 0.06, title="") {
     geom_smooth(aes(weight = snp_count), method = "lm", linetype = 2, fullrange = TRUE, size = 0.5) +
     xlim(40000, 0) + ylim(0, ymax) +
     xlab("years before present") + ylab("Neandertal ancestry proportion") +
-    ggtitle(title)
+    ggtitle(title) +
+    paper_theme(legend.position = "none")
 }
 
 # Near East populations to exclude from West Eurasians in plots over time
 near_east <- c("BedouinB", "Druze", "Iranian", "Iraqi_Jew", "Jordanian",
                "Palestinian", "Samaritan", "Turkish", "Yemenite_Jew")
-
 ignore_samples <- c(near_east, "UstIshim")
 
-# new direct f4 ratio estimates using Altai and Vindija
-direct_f4_all <- readRDS("rds/direct_f4_combined.rds") %>% filter(!X %in% ignore_samples)
-direct_f4_bigyri <- readRDS("rds/direct_f4_BigYRI.rds") %>% filter(!X %in% ignore_samples)
-direct_f4_ho <- readRDS("rds/direct_f4_HO.rds") %>% filter(!X %in% ignore_samples)
+# load the estimates
+admix_array <- readRDS("rds/admixture_array_prop.rds") %>% filter(!X %in% ignore_samples) %>% mutate(stat = "admix_prop")
+f4_nea <- readRDS("rds/f4_nea.rds") %>% filter(!X %in% ignore_samples)
 
-# indirect f4 ratio estimates based on assumptions about African populations
-indirect_f4_all <- readRDS("rds/indirect_f4_combined.rds") %>% filter(!X %in% ignore_samples)
-indirect_f4_bigyri <- readRDS("rds/indirect_f4_BigYRI.rds") %>% filter(!X %in% ignore_samples)
-indirect_f4_ho <- readRDS("rds/indirect_f4_HO.rds") %>% filter(!X %in% ignore_samples)
+nea_est <- bind_rows(admix_array, f4_nea) %>%
+  select(X, stat, alpha, Zscore, snp_count, age, pop, stderr, sites)
 
-# admixture array estimates
-admix_prop <- readRDS("rds/admixture_array_prop.rds") %>% filter(!X %in% ignore_samples)
 
-# plots
-
-indirect_f4_all %>%
-  plot_nea_time(snp_cutoff = 200000, title = "Ice Age indirect f4 ratio") +
-  paper_theme(legend.position = "none")
+plot_nea_time(nea_est, statistic = "indirect_f4", array = "all",
+              snp_cutoff = 1000, title = "Direct f4 ratio")
 ggsave("fig/indirect_f4.png")
 
-admix_prop %>%
-  plot_nea_time(title = "Proportion of Neandertal-like alleles") +
-  paper_theme(legend.position = "none")
-ggsave("fig/admix_prop.png")
+plot_nea_time(nea_est, statistic = "direct_f4", array = "all",
+              snp_cutoff = 1000, title = "Direct f4 ratio")
+ggsave("fig/direct_f4.png")
 
-filter(direct_f4_all, C == "Dinka") %>%
-  plot_nea_time(snp_cutoff = 200000, title = "Direct f4 ratio") +
-  paper_theme(legend.position = "none")
-ggsave("fig/direct_f4_all.png")
-
-filter(direct_f4_bigyri, C == "Dinka") %>%
-  plot_nea_time(snp_cutoff = 50000, title = "Direct f4 ratio (Big Yoruba sites)") +
-  paper_theme(legend.position = "none")
-ggsave("fig/direct_f4_bigyri.png")
-
-filter(direct_f4_ho, C == "Dinka") %>%
-  plot_nea_time(snp_cutoff = 150000, title = "Direct f4 ratio (Human Origins sites)") +
-  paper_theme(legend.position = "none")
-ggsave("fig/direct_f4_ho.png")
-
-
+plot_nea_time(nea_est, statistic = "admix_prop", array = "admixture_array",
+              snp_cutoff = 1000, title = "Admixture array proportion")
+ggsave("fig/admix_array_prop.png")
 
 
 
@@ -87,7 +82,42 @@ ggsave("fig/direct_f4_ho.png")
 
 # Affinities over time to different SGDP populations ----------------------
 
-afr_affinities <- readRDS("rds/african_affinities.rds")
+
+
+
+# Grouped African populations ---------------------------------------------
+
+sgdp_affinities <- readRDS("rds/sgdp_affinities_ui_chimp.rds")
+
+sgdp_affinities %>% filter(Y %in% c("EastAfrica", "CentralAfrica", "WestAfrica"), n_snps > 50000) %>%
+  mutate(Y = paste0("Y = ", Y)) %>% 
+  mutate(Y = str_replace(Y, "Africa", " Afr.")) %>% 
+  ggplot(aes(age, D)) + geom_point(aes(shape = abs(Zscore) > 3), size = 5, alpha = 1/5) +
+  geom_hline(yintercept = 0, linetype = 2, color = "blue") +
+  xlim(40000, -2000) + ylim(-0.05, 0.02) +
+  xlab("years before present") + ylab("D statistic") +
+  ggtitle("D(Ust'-Ishim, West Eurasian; African Y, Chimp)") +
+  facet_grid(. ~ Y) +
+  scale_shape_manual(values = c(21, 19)) +
+  paper_theme(legend.position = "none", axis.text.x = element_text(hjust = 1, angle = 30))
+ggsave("fig/sgdp_affinities_ui_chimp.png")
+
+sgdp_affinities %>% filter(Y == "Oceania") %>%
+  ggplot(aes(age, Zscore)) + geom_point(aes(shape = abs(Zscore) > 3), size = 5, alpha = 1/2) +
+  geom_hline(yintercept = 0, linetype = 2, color = "blue") +
+  geom_hline(yintercept = c(-3, 3), linetype = 2, color = "red") +
+  xlim(40000, -2000) + ylim(-20, 20) +
+  xlab("years before present") + ylab("Z score") +
+  ggtitle("D(Ust'-Ishim, West Eurasian; Oceanians, Chimp)") +
+  scale_shape_manual(values = c(21, 19)) +
+  paper_theme(legend.position = "none", axis.text.x = element_text(hjust = 1, angle = 30))
+ggsave("fig/african_affinities_ui_chimp_oceania.png")
+
+
+
+# Individual Yoruba, Dinka and Mbuti --------------------------------------
+
+afr_affinities <- readRDS("rds/african_affinities_ui_chimp.rds")
 
 afr_affinities %>% filter(Y %in% c("Mbuti", "Dinka", "Yoruba"), n_snps > 50000) %>%
   mutate(Y = paste0("Y = ", Y)) %>% 
@@ -100,22 +130,48 @@ afr_affinities %>% filter(Y %in% c("Mbuti", "Dinka", "Yoruba"), n_snps > 50000) 
   facet_grid(. ~ Y) +
   scale_shape_manual(values = c(21, 19)) +
   paper_theme(legend.position = "none", axis.text.x = element_text(hjust = 1, angle = 30))
-ggsave("fig/affinities_africa.png")
-
-afr_affinities %>% filter(Y == "Oceania") %>%
-  ggplot(aes(age, Zscore)) + geom_point(aes(shape = abs(Zscore) > 3), size = 5, alpha = 1/2) +
-  geom_hline(yintercept = 0, linetype = 2, color = "blue") +
-  geom_hline(yintercept = c(-3, 3), linetype = 2, color = "red") +
-  xlim(40000, -2000) + ylim(-20, 20) +
-  xlab("years before present") + ylab("Z score") +
-  ggtitle("D(Ust'-Ishim, West Eurasian; Oceanians, Chimp)") +
-  scale_shape_manual(values = c(21, 19)) +
-  paper_theme(legend.position = "none", axis.text.x = element_text(hjust = 1, angle = 30))
-ggsave("fig/affinities_oceania.png")
+ggsave("fig/african_affinities_ui_chimp.png")
 
 
 
 
+
+near_east_proportion <- readRDS("rds/nea_east_proportions.rds")
+
+near_east_proportion %>% filter(B == "Levant_N") %>%
+  filter(snp_count > 200000) %>% 
+  ggplot(aes(age, alpha)) +
+  geom_point(size=3, alpha=1/2) +
+  geom_errorbar(aes(ymin=alpha - 2 * stderr, ymax=alpha + 2 * stderr)) +
+  geom_hline(yintercept=c(0, 1), linetype=2, color="red") +
+  facet_wrap(~ A) +
+  xlim(40000, 0) + labs(y="proportion of ancient Near East ancestry") +
+  theme(legend.position="none", axis.text.x=element_text(angle=45, hjust=1)) +
+  ggtitle("Near East ancestry in Ice Age samples and SGDP Europeans over time",
+          subtitle="Calculated on the 2.2M sites array, as a ratio of f4 statistics from the equation above")
+
+
+near_east_proportion %>% filter(B == "Levant_N", A == "BedouinB") %>%
+  filter(snp_count > 200000) %>%
+  ggplot(aes(age, alpha)) +
+  geom_point(size=3, alpha=1/2) +
+  geom_errorbar(aes(ymin=alpha - 3 * stderr, ymax=alpha + 3 * stderr)) +
+  geom_hline(yintercept=c(0, 1), linetype=2, color="red") +
+  xlim(40000, 0) + labs(y="proportion of ancient Near East ancestry") +
+  ggtitle("Proportion of ancient Near East ancestry in Europeans over time") +
+  theme(legend.position="none", axis.text.x=element_text(angle=45, hjust=1))
+
+
+
+
+bin_props <- readRDS("rds/depletion_near_genes.rds")
+
+bin_props %>% filter(gen == 2200) %>% 
+  ggplot(aes(factor(dist_bin), nea_prop)) +
+  geom_jitter(alpha = 1/5, size = 0.2) +
+  geom_boxplot(alpha = 0.8) +
+  geom_smooth(aes(group = 1), method = "lm", linetype = 2, color = "red") +
+  paper_theme()
 
 
 
@@ -128,56 +184,6 @@ deltas <- readRDS("rds/deltas.rds")
 
 
 # Deleterious delta plots -------------------------------------------------
-
-s_breaks <- c(0, -3, -5, -Inf)
-s_labels <- as.character(10^s_breaks)
-s_bins <- rev(paste0("s = ", s_labels[-length(s_labels)], paste0(" - ", s_labels[-1])))
-deltas %>% mutate(logS_bin=cut(logS,
-                               breaks=s_breaks,
-                               labels=s_bins)) %>%
-  replace_na(list(logS_bin="s = 1e-05 - 0")) %>% 
-  mutate(logS_bin=factor(logS_bin, levels=rev(s_bins))) %>%
-  group_by(g, mut_type, rep, logS_bin) %>%    
-  summarise(avg_delta = mean(delta_f), sd_delta = sd(delta_f), n_delta = n()) %>%
-  mutate(se_delta = sd_delta / sqrt(n_delta),
-         lower_ci = avg_delta - qt(1 - (0.05 / 2), n_delta - 1) * se_delta,
-         upper_ci = avg_delta + qt(1 - (0.05 / 2), n_delta - 1) * se_delta) %>% 
-  filter(g >= 100) %>% 
-  filter(mut_type %in% c("MH_del", "Nea_del")) %>% 
-  ungroup %>% 
-  mutate(mut_type = str_replace(mut_type, "MH_del", "modern human") %>%
-                    str_replace("Nea_del", "introgressed")) %>% 
-  ggplot(aes(g, avg_delta)) +
-  geom_line(aes(color = rep), size = 1, alpha = 1/2) +
-  geom_smooth(color = "black", alpha = 1/2) + 
-  geom_hline(yintercept = 0, linetype = 2, size = 0.25) + facet_grid(mut_type ~ logS_bin) +
-  xlab("generation since admixture") + ylab("average frequency change") +
-  paper_theme(legend.position = "none", axis.text.x = element_text(hjust = 1, angle = 30))
-ggsave("fig/deltas_deleterious.png")
-
-# Neutral marker delta plots ----------------------------------------------
-
-group_by(deltas, g, mut_type, rep) %>%    
-  summarise(avg_delta=mean(delta_f), sd_delta=sd(delta_f), n_delta=n()) %>%
-  mutate(se_delta=sd_delta / sqrt(n_delta),
-         lower_ci=avg_delta - qt(1 - (0.05 / 2), n_delta - 1) * se_delta,
-         upper_ci=avg_delta + qt(1 - (0.05 / 2), n_delta - 1) * se_delta) %>% 
-  filter(g >= 100) %>% 
-  ungroup %>% 
-  filter(mut_type %in% c("gap_marker", "region_marker")) %>% 
-  mutate(mut_type = str_replace(mut_type, "gap_marker", "neutral markers outside selected regions") %>% 
-                    str_replace("region_marker", "neutral markers within selected regions")) %>% 
-  ggplot(aes(g, avg_delta)) +
-  geom_line(aes(color = rep), size = 1, alpha = 1/2) +
-  geom_smooth(color = "black", alpha = 1/2) + 
-  geom_hline(yintercept = 0, color = "blue", linetype = 2) +
-  facet_grid(. ~ mut_type) + 
-  xlab("generation since admixture") +
-  ylab("average frequency change per generation") +
-  paper_theme(legend.position = "none", axis.text.x = element_text(hjust = 1, angle = 30))
-ggsave("fig/deltas_neutral.png")
-
-
 
 
 
