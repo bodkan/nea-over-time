f7414a915ecf9e5d619c32773fc5ed9286f6745d
Martin Petr <git@bodkan.net>
2018-06-28 16:21:16 +0200

    Simulate samples uniformly spaced in time

--- 03_coalsims.sh
+++ 03_coalsims.sh
@@ -5,14 +5,16 @@ mkdir data/coalsims
 t=20_000
 m=0.0001
 
+sample_times=`seq 0 1000 50000`
+
 # no EUR <-> AFR migration
 for rep in `seq 1 100`; do
     N="no_migration_${rep}"
     qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time 0 --eur-to-afr 0 --afr-to-eur 0 --nea-rate 0.03 \
-            --hap-length 500_000_000 --emh-ages data/emh_ages.txt \
-            --output-file data/coalsims/${N}.tsv
+            --hap-length 500_000_000 --eur-ages ${sample_times} \
+            --output-file data/coalsims/${N}.tsv --calc-stats
 done
 
 # EUR -> AFR migration
@@ -21,8 +23,8 @@ for rep in `seq 1 100`; do
     qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time ${t} --eur-to-afr ${m} --afr-to-eur 0 --nea-rate 0.03 \
-            --hap-length 500_000_000 --emh-ages data/emh_ages.txt \
-            --output-file data/coalsims/${N}.tsv
+            --hap-length 500_000_000 --eur-ages ${sample_times} \
+            --output-file data/coalsims/${N}.tsv --calc-stats
 done
 
 
@@ -32,8 +34,8 @@ for rep in `seq 1 100`; do
     qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time ${t} --eur-to-afr 0 --afr-to-eur ${m} --nea-rate 0.03 \
-            --hap-length 500_000_000 --emh-ages data/emh_ages.txt \
-            --output-file data/coalsims/${N}.tsv
+            --hap-length 500_000_000 --eur-ages ${sample_times} \
+            --output-file data/coalsims/${N}.tsv --calc-stats
 done
 
 # EUR <-> AFR migration
@@ -42,6 +44,36 @@ for rep in `seq 1 100`; do
     qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time ${t} --eur-to-afr ${m} --afr-to-eur ${m} --nea-rate 0.03 \
-            --hap-length 500_000_000 --emh-ages data/emh_ages.txt \
-            --output-file data/coalsims/${N}.tsv
+            --hap-length 500_000_000 --eur-ages ${sample_times} \
+            --output-file data/coalsims/${N}.tsv --calc-stats
 done
+
+
+#
+# SNP table dumps for investigating BABA/ABBA changes
+#
+t=20_000
+m=0.0001
+sample_times=`seq 0 1000 50000`
+
+./code/coalsim.py \
+    --time ${t} --eur-to-afr 0 --afr-to-eur 0 --nea-rate 0.03 \
+    --hap-length 500_000_000 --eur-ages ${sample_times} \
+    --output-file data/coalsims/snps_no_migration.tsv --dump-snps &
+./code/coalsim.py \
+    --time ${t} --eur-to-afr ${m} --afr-to-eur 0 --nea-rate 0.03 \
+    --hap-length 500_000_000 --eur-ages ${sample_times} \
+    --output-file data/coalsims/snps_eur_to_afr.tsv --dump-snps &
+./code/coalsim.py \
+    --time ${t} --eur-to-afr 0 --afr-to-eur ${m} --nea-rate 0.03 \
+    --hap-length 500_000_000 --eur-ages ${sample_times} \
+    --output-file data/coalsims/snps_afr_to_eur.tsv --dump-snps &
+./code/coalsim.py \
+    --time ${t} --eur-to-afr ${m} --afr-to-eur ${m} --nea-rate 0.03 \
+    --hap-length 500_000_000 --eur-ages ${sample_times} \
+    --output-file data/coalsims/snps_both_directions.tsv --dump-snps &
+./code/coalsim.py \
+    --time ${t} --eur-to-afr ${m} --afr-to-eur 0 --nea-rate 0 \
+    --hap-length 500_000_000 --eur-ages ${sample_times} \
+    --output-file data/coalsims/snps_eur_to_afr_no_nea.tsv --dump-snps &
+
