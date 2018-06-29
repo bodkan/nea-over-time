9d77ae567030bcaf7073087111cbc32f52a0bd15
Martin Petr <git@bodkan.net>
2018-06-29 11:58:02 +0200

    Increase memory requirement

--- 03_coalsims.sh
+++ 03_coalsims.sh
@@ -10,7 +10,7 @@ sample_times=`seq 0 1000 50000`
 # no EUR <-> AFR migration
 for rep in `seq 1 100`; do
     N="no_migration_${rep}"
-    qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
+    qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time 0 --eur-to-afr 0 --afr-to-eur 0 --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
@@ -20,7 +20,7 @@ done
 # EUR -> AFR migration
 for rep in `seq 1 100`; do
     N="eur_to_afr_${rep}"
-    qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
+    qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time ${t} --eur-to-afr ${m} --afr-to-eur 0 --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
@@ -31,7 +31,7 @@ done
 # EUR <- AFR migration
 for rep in `seq 1 100`; do
     N="afr_to_eur_${rep}"
-    qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
+    qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time ${t} --eur-to-afr 0 --afr-to-eur ${m} --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
@@ -41,7 +41,7 @@ done
 # EUR <-> AFR migration
 for rep in `seq 1 100`; do
     N="both_directions_${rep}"
-    qsub -V -cwd -j y -l virtual_free=20G,h_vmem=20G -N $N -o tmp/${N}.txt \
+    qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
             --time ${t} --eur-to-afr ${m} --afr-to-eur ${m} --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
