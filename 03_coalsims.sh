4987efdf99bdfd3246dca03365e4796f9d7838ed
Martin Petr <git@bodkan.net>
2018-07-20 01:27:58 +0200

    Calculate stats on ascertained SNPs

--- 03_coalsims.sh
+++ 03_coalsims.sh
@@ -11,42 +11,42 @@ m=0.0001
 sample_times=`seq 50000 -1000 0`
 
 # no EUR <-> AFR migration
-for rep in `seq 1 100`; do
+for rep in `seq 1 20`; do
     N="symmetry_no_migration_${rep}"
     qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
-            --time 0 --eur-to-afr1 0 --eur-to-afr2 0 --afr-to-eur 0 --nea-rate 0.03 \
+            --time 0 --eur-to-afr 0 --afr-to-eur 0 --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
             --output-file data/msprime_sims/${N}.tsv --calc-stats
 done
 
 # EUR -> AFR migration
-for rep in `seq 1 100`; do
+for rep in `seq 1 20`; do
     N="symmetry_eur_to_afr_${rep}"
     qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
-            --time ${t} --eur-to-afr1 ${m} --eur-to-afr2 ${m} --afr-to-eur 0 --nea-rate 0.03 \
+            --time ${t} --eur-to-afr ${m} --afr-to-eur 0 --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
             --output-file data/msprime_sims/${N}.tsv --calc-stats
 done
 
 
 # EUR <- AFR migration
-for rep in `seq 1 100`; do
+for rep in `seq 1 20`; do
     N="symmetry_afr_to_eur_${rep}"
     qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
-            --time ${t} --eur-to-afr1 0 --eur-to-afr2 0 --afr-to-eur ${m} --nea-rate 0.03 \
+            --time ${t} --eur-to-afr 0 --afr-to-eur ${m} --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
             --output-file data/msprime_sims/${N}.tsv --calc-stats
 done
 
 # EUR <-> AFR migration
-for rep in `seq 1 100`; do
+for rep in `seq 1 20`; do
     N="symmetry_both_directions_${rep}"
     qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
         ./code/coalsim.py \
-            --time ${t} --eur-to-afr1 ${m} --eur-to-afr2 ${m} --afr-to-eur ${m} --nea-rate 0.03 \
+            --time ${t} --eur-to-afr ${m} --afr-to-eur ${m} --nea-rate 0.03 \
             --hap-length 500_000_000 --eur-ages ${sample_times} \
             --output-file data/msprime_sims/${N}.tsv --calc-stats
 done
