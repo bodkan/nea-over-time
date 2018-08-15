72c640cb1685d903c1d205620744c1bf08b36f70
Martin Petr <git@bodkan.net>
2018-08-15 13:19:03 +0200

    Update coalsim pipeline

--- 03_coalsims.sh
+++ 03_coalsims.sh
@@ -6,14 +6,14 @@ sample_times=`seq 50000 -2000 0`
 
 r=0.05
 
+for asc in all yoruba eur eas; do
 for t in 2000 5000 10000 15000 20000; do
+    scenario="ascertainment_${asc}_geneflow_both_afr_${t}"
 
     m=`echo "$r / ($t / 25)" | bc -l`
 
-    scenario="geneflow_both_afr_${t}"
-
     # EUR -> AFR migration
-    for rep in `seq 1 100`; do
+    for rep in `seq 1 50`; do
         N="${scenario}_eur_to_afr_${rep}"
         qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
             ./code/coalsim.py \
@@ -24,7 +24,7 @@ for t in 2000 5000 10000 15000 20000; do
     
     
     # EUR <- AFR migration
-    for rep in `seq 1 100`; do
+    for rep in `seq 1 50`; do
         N="${scenario}_afr_to_eur_${rep}"
         qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
             ./code/coalsim.py \
@@ -34,7 +34,7 @@ for t in 2000 5000 10000 15000 20000; do
     done
     
     # EUR <-> AFR migration
-    for rep in `seq 1 100`; do
+    for rep in `seq 1 50`; do
         N="${scenario}_both_directions_${rep}"
         qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
             ./code/coalsim.py \
@@ -44,7 +44,7 @@ for t in 2000 5000 10000 15000 20000; do
     done
 
     # no EUR <-> AFR migration
-    for rep in `seq 1 100`; do
+    for rep in `seq 1 50`; do
         N="${scenario}_no_migration_${rep}"
         qsub -V -cwd -j y -l virtual_free=30G,h_vmem=30G -N $N -o tmp/${N}.txt \
             ./code/coalsim.py \
@@ -53,4 +53,5 @@ for t in 2000 5000 10000 15000 20000; do
                 --output-file data/msprime_sims/${N}.tsv --calc-stats
     done
     
+done
 done
